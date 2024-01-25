2ecca1df92318638709549842457232e530fc615
Kalpitrao47 <kalpitraorane47@gmail.com>
2024-01-25 18:33:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import FormsStatic from './components/FormsStatic';
+import FormsDynamicData from './components/FormsDynamicData';
 
 function App() {
+  const [options,setOptions] = useState([]);
+  console.log("App DAta",options)
+  
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async () =>{
+    const data = await fetch ("https://jsonplaceholder.typicode.com/albums")
+
+    const json = await data.json();
+    console.log("json data",json);
+    setOptions(json);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/*  <FormsStatic/>   */}
+        <FormsDynamicData options={options}/>
     </div>
   );
 }
---
+++ src/components/FormsDynamicData.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+
+const FormsDynamicData = ({ options }) => {
+  console.log("data", options);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState('');
+  const [option, setOption] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("name", name);
+    console.log("email", email);
+    console.log("option", option);
+    const formData = {
+      name: name,
+      email: email,
+      option: option,
+    };
+
+    console.log("Form Data:", formData);
+    setName("");
+    setEmail("");
+    setOption("");
+  };
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="p-2">
+            <label>Name:</label>
+            <input
+              type="text"
+              className="border border-black p-2 rounded-lg"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Email:</label>
+            <input
+              className="border border-black p-2 rounded-lg"
+              placeholder="Email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="p-2">
+            <label>Select Division:</label>
+            <select
+              onChange={(e) => setOption(e.target.value)}
+              className="border border-black rounded-lg"
+            >
+              {options.map((option) => (
+                <option key={option.id} value={option.title}>
+                  {option.title}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <button
+            className="border border-black mt-4 rounded-lg p-2"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormsDynamicData;
